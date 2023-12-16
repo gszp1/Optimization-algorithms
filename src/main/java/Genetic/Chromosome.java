5f4551a799c259c5b4a6f9d5777ace154320eafa
gszp1 <grzybekmateusz@poczta.fm>
2023-12-16 23:21:39 +0100

    Added chromosome class

---
+++ src/main/java/Genetic/Chromosome.java
@@ -0,0 +1,34 @@
+package Genetic;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Chromosome {
+
+    public static StringBuilder generateBinaryVector(int length) {
+        StringBuilder binaryVector = new StringBuilder();
+        for(int i = 0; i < length; ++i) {
+            binaryVector.append(Integer.toString(ThreadLocalRandom.current().nextInt(0, 2)));
+        }
+        return binaryVector;
+    }
+
+
+    private final StringBuilder x;
+
+    private final StringBuilder y;
+
+    public Chromosome(int length) {
+        x = generateBinaryVector(length);
+        y = generateBinaryVector(length);
+    }
+
+    public StringBuilder getX() {
+        return x;
+    }
+
+    public StringBuilder getY() {
+        return y;
+    }
+}
--- src/main/java/Genetic/GeneticAlgorithm.java
+++ src/main/java/Genetic/GeneticAlgorithm.java
@@ -1,4 +1,49 @@
 package Genetic;
 
+import utils.Function;
+
+import java.util.ArrayList;
+
 public class GeneticAlgorithm {
+
+    private final double minX;
+
+    private final double maxX;
+
+    private final double minY;
+
+    private final double maxY;
+
+    private final float crossingProbability;
+
+    private final float mutationProbability;
+
+    private final int populationSize;
+
+    private final int precision;
+
+    private final ArrayList<Chromosome> chromosomes;
+
+    private final Function function;
+
+    public GeneticAlgorithm(float crossingProbability, float mutationProbability, int populationSize,
+                            double minX, double maxX, double minY, double maxY, int precision, Function function) {
+        this.crossingProbability = crossingProbability;
+        this.mutationProbability = mutationProbability;
+        this.populationSize = populationSize;
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minY = minY;
+        this.maxY = maxY;
+        this.precision = precision;
+        chromosomes = new ArrayList<>();
+
+    }
+
+    public static void main(String [] args) {
+        Chromosome chromosome = new Chromosome(15);
+        System.out.println(chromosome.getX());
+        System.out.println(chromosome.getY());
+    }
+
 }
