58431ff543825d973948b8e0f52dbbf3d7baddd1
gszp1 <grzybekmateusz@poczta.fm>
2023-12-17 21:17:11 +0100

    Added domain borders validation

--- src/main/java/utils/AlgorithmPicker.java
+++ src/main/java/utils/AlgorithmPicker.java
@@ -4,6 +4,7 @@ import Genetic.GeneticAlgorithm;
 import ParticleSwarm.ParticleSwarmAlgorithm;
 import ParticleSwarm.Position;
 
+import java.text.ParseException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,9 +14,22 @@ public class AlgorithmPicker {
         if (args.length < 6) {
             System.out.println("Not enough parameters provided.\n"
                     + "Required parameters: Algorithm, xMin, xMax, yMin, yMax, function");
+            return;
         }
-        String function = String.join(" ", args);
-        System.out.println(function);
+        try {
+            double xMin = Integer.parseInt(args[1]);
+            double xMax = Integer.parseInt(args[2]);
+            double yMin = Integer.parseInt(args[3]);
+            double yMax = Integer.parseInt(args[4]);
+            if (xMin >= xMax || yMin >= yMax) {
+                System.out.println("Provided x or y domains are incorrect.");
+                return;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Provided x or y domains are incorrect.");
+            return;
+        }
+        String function = String.join("", args);
         Function f = new Function(function);
 //        ParticleSwarmAlgorithm particleSwarm = new ParticleSwarmAlgorithm(0, 100, 0, 100, 100, f,
 //                1000, 0.5, 0.5, 0.5);
