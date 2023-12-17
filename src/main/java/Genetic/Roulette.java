df6d7e605630ae9cd1a9c1ad7302a06b0ae3092f
gszp1 <grzybekmateusz@poczta.fm>
2023-12-17 01:40:16 +0100

    Added RouletteFields handling methods

--- src/main/java/Genetic/Roulette.java
+++ src/main/java/Genetic/Roulette.java
@@ -1,4 +1,29 @@
 package Genetic;
 
+import java.util.ArrayList;
+
 public class Roulette {
+
+    private double currentUpperBoundary = 0;
+
+    private final ArrayList<RouletteField> rouletteFields;
+
+    public Roulette() {
+        rouletteFields = new ArrayList<>();
+    }
+    
+    public Chromosome findChromosome(double fitnessIndex) {
+        for (RouletteField field: rouletteFields) {
+            if (fitnessIndex >= field.getLowerBoundary() && fitnessIndex < field.getUpperBoundary()) {
+                return field.getChromosome();
+            }
+        }
+        return null;
+    }
+
+    public void addRouletteField(Chromosome chromosome, double fitnessIndex) {
+        rouletteFields.add(new RouletteField(currentUpperBoundary, currentUpperBoundary + fitnessIndex, chromosome));
+        currentUpperBoundary += fitnessIndex;
+    }
+
 }
